99c345ae43d62448c451bd399bac3608227a7912
winter3000 <Ruoyu384@outlook.com>
2017-05-06 17:58:54 +0800

    添加

--- js/leisurearea.js
+++ js/leisurearea.js
@@ -1,8 +1,10 @@
 /**
- * Created by Ruoyu on 2017/5/2.
+ * Created by Ruoyu on 2017/5/4.
  */
 $(function(){
     //设置主体高
     var windowW=$(window).width();
     $('#mainpart').height(windowW/1920*895+'px');
+    //遮罩
+
 });
--- js/model.js
+++ js/model.js
@@ -5,5 +5,15 @@ $(function(){
     //底部按钮
     $('#footer .button').click(function(){
         $('#footer .main').toggle('500');
-    })
+    });
+    //图片与鼠标反向旋转
+    var windowW=$(window).width();
+    var frontBg=$('#mainpart .front');
+    var laterBg=$('#mainpart .later');
+    $('#mainpart').mousemove(function(e){
+        var distance=e.pageX-windowW/2;    //距离差
+        var  distanceRatio=distance/windowW*2; //距离差比率-1~1
+        frontBg.css('transform','rotateY('+distanceRatio*8+'deg)');
+        laterBg.css('transform','rotateY('+distanceRatio*15+'deg)');
+    });
 });
