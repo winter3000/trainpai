f0cef01e3acfed095e6bf4428d27ef9ac8442e0a
winter3000 <Ruoyu384@outlook.com>
2017-05-20 11:41:13 +0800

    05.20

--- js/model.js
+++ js/model.js
@@ -2,12 +2,8 @@
  * Created by Ruoyu on 2017/5/2.
  */
 $(function(){
-    //底部按钮
-    $('#footer .button').click(function(){
-        $('#footer .main').toggle('500');
-    });
     //图片与鼠标反向旋转
-    var windowW=$(window).width();
+/*    var windowW=$(window).width();
     var frontBg=$('#mainpart .front');
     var laterBg=$('#mainpart .later');
     $('#mainpart').mousemove(function(e){
@@ -15,5 +11,5 @@ $(function(){
         var  distanceRatio=distance/windowW*2; //距离差比率-1~1
         frontBg.css('transform','rotateY('+distanceRatio*10+'deg)');
         laterBg.css('transform','rotateY('+distanceRatio*20+'deg)');
-    });
+    });*/
 });
